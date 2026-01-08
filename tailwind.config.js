a176823efa961205485fb1bbcc2d180bc5b50c47
pratu1218 <pratikbhosle1209@gmail.com>
2026-01-08 22:48:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // tell Tailwind to scan all JSX files inside src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
